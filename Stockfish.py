b40a357f66923336e1ddbf6543251fae9e18a86a
OriolCamps33 <oriolcamps20@gmail.com>
2024-06-05 19:29:03 +0200

    Stock+Leela

---
+++ Stockfish.py
@@ -0,0 +1,19 @@
+import chess
+import chess.engine
+
+
+class Stockfish:
+    def __init__(self, url, depth=5) -> None:
+
+        self.engine = chess.engine.SimpleEngine.popen_uci(url)
+        self.limit = chess.engine.Limit(depth=depth)
+
+    def get_legal_moves(self, board):
+
+        return list(board.legal_moves)
+    
+    def choose_action(self, board, legal_moves):
+        result = self.engine.play(board, self.limit)
+        return result.move
+
+        
--- proves.py
+++
@@ -1,4 +0,0 @@
-import pygame
-
-x = 2
-print(chr(96+x)) 
